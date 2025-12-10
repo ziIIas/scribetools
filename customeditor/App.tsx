05a50208f768ac15a2ae2aed12b3dab4ed2f891a
zilla <51808001+ziIIas@users.noreply.github.com>
2025-12-09 21:10:20 -0500

    a

---
+++ customeditor/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import GeniusEditor from './components/GeniusEditor';
+
+const App: React.FC = () => {
+  return (
+    <div style={{ width: '100%', height: '100%' }}>
+      <GeniusEditor />
+    </div>
+  );
+};
+
+export default App;
